b24a07bae18352e90a57c2389e1958056282d650
Susan Xiao <shxiao@students.pitzer.edu>
2019-02-04 16:19:12 -0800

    Update server.py
    updated url

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Gu4Kvb_sWj11uWvpHHklSv9Ohjav_xF2'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MTmXbt9BpZK4u4iyQBdieBVHVpjeNv-5'
 export_file_name = 'export.pkl'
 
 classes = ['watercolor', 'pencilsketch', 'chinesepainting']
